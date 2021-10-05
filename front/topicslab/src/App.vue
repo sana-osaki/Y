6898aacd10526bdaba41f6569761dd3292a943ec
Hana-Ueda <11sabonen30@gmail.com>
2021-10-05 10:27:52 +0900

    aaa

--- front/topicslab/src/App.vue
+++ front/topicslab/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Ho6me</router-link> |
     <template v-if="authenticated">
       <a>mypage</a>
     </template>
