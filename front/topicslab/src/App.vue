caee4cfbda51708f4256d96f486d86cee29700fb
Hana-Ueda <11sabonen30@gmail.com>
2021-10-05 14:34:44 +0900

    宿題にしました

--- front/topicslab/src/App.vue
+++ front/topicslab/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="nav">
-    <router-link to="/">HomeWork</router-link> |
+    <router-link to="/">1HOMEWORK1</router-link> |
     <template v-if="authenticated">
       <a>mypage</a>
     </template>
