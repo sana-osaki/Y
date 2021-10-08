dd978fc89fcc6397764cdfe14acf3e6aea68a148
sana-osaki <sagdo6322@gmail.com>
2021-10-08 14:17:04 +0900

    項目２のエラーを修正しました

--- api/topicsLab/app/Http/Controllers/UserController.php
+++ api/topicsLab/app/Http/Controllers/UserController.php
@@ -52,12 +52,12 @@ class UserController extends Controller
      * @return \Illuminate\Http\Response
      */
      public function show(User $user)
-    {
+     {
         // return User::with('topics.user', 'comments.user')->find($user->id);
         return User::with('topics', 'comments')->find($user->id);
         // return $user;
         // return User::with('topics.title' , 'comments.title')->find($user->id);
-    }
+     }
 
     /**
      * Show the form for editing the specified resource.
